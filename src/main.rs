92cd164f5a3bc56f59bfcd93f887400e9dd2a6f7
Poogee <kortunoov@bk.ru>
2022-10-20 06:46:13 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,23 @@ use ndarray::{arr2, Array};
 #[allow(non_upper_case_globals)]
 
 const n:usize = 10;
+fn shesh(frame_n: i64) -> usize{
+    if frame_n <= 160{ return 0 }
+    let a = frame_n as usize % (8*n) ;
+    if a > 6*n {return 0}
+    if a > 3*n {return 6*n - a}
+    
+    a
 
-fn cube_coords() -> Vec<Vec<f64>> {
+}
+fn cube_coords(frame_n: i64) -> Vec<Vec<f64>> {
     let mut cube_coords = Vec::new();
     for x in 0..10*n{
-        for y in 0..10*n{
+        for y in 0..13*n{
             for z in 0..10*n{
-
+                
                 if 4*n <= x && x <= 5*n{
-                    if y == 4*n-1{
+                    if y == (4*n-1) - shesh(frame_n) {
                         if 4*n <= z && z <= 5*n{
                             cube_coords.push(vec![x as f64, y as f64, z as f64, 0f64])
                         }
@@ -23,16 +31,16 @@ fn cube_coords() -> Vec<Vec<f64>> {
                 }
 
 
-                if 4*n <= y && y <= 5*n{
-                    if x == 4*n-1{
+                if (4*n) <= y && y <= (5*n){
+                    if x == 4*n - shesh(frame_n){
                         if 4*n <= z && z <= 5*n{
                             cube_coords.push(vec![x as f64, y as f64, z as f64, 1f64])
                         }
                     }
                 }
 
-                if 4*n <= y && y <= 5*n{
-                    if z == 4*n-1{
+                if (4*n) <= y && y <= (5*n) {
+                    if z == 4*n - shesh(frame_n){
                         if 4*n <= x && x <= 5*n{
                             cube_coords.push(vec![x as f64, y as f64, z as f64, 2f64])
                         }
@@ -40,7 +48,7 @@ fn cube_coords() -> Vec<Vec<f64>> {
                 }
 
                 if 4*n <= x && x <= 5*n{
-                    if y == 5*n{
+                    if y == (5*n) + shesh(frame_n) {
                         if 4*n <= z && z <= 5*n{
                             cube_coords.push(vec![x as f64, y as f64, z as f64, 3f64])
                         }
@@ -48,20 +56,90 @@ fn cube_coords() -> Vec<Vec<f64>> {
                 }
 
 
-                if 4*n <= y && y <= 5*n{
-                    if x == 5*n{
+                if (4*n) <= y && y <= (5*n) {
+                    if x == 5*n + shesh(frame_n){
                         if 4*n <= z && z <= 5*n{
                             cube_coords.push(vec![x as f64, y as f64, z as f64, 4f64])
                         }
                     }
                 }
 
-                if 4*n <= y && y <= 5*n{
-                    if z == 5*n{
+                if (4*n) <= y && y <= (5*n) {
+                    if z == 5*n + shesh(frame_n){
                         if 4*n <= x && x <= 5*n{
                             cube_coords.push(vec![x as f64, y as f64, z as f64, 5f64])
                         }
                     }
+                }
+                if frame_n >= 400{
+                    if (x+y==10){cube_coords.push(vec![x as f64, y as f64, z as f64, 5f64]) }
+
+
+                }
+                if frame_n >= 460{
+                    if (x as i64-5)*(x as i64-5) + (y as i64-5)*(y as i64-5) + (z as i64-20)*(z as i64-20) <= (26*n*n) as i64{
+                        if (x as i64-5)*(x as i64-5) + (y as i64-5)*(y as i64-5) + (z as i64-20)*(z as i64-20) >= (24*n*n) as i64{
+                        cube_coords.push(vec![x as f64, y as f64, z as f64, 5f64]) 
+                    }
+                }
+                }
+
+                if frame_n >= 320{
+                    if 0 <= x && x <= 3*n{
+                        if y == (0) {
+                            if 0 <= z && z <= 3*n{
+                                cube_coords.push(vec![x as f64, y as f64, z as f64, 0f64])
+                            }
+                        }
+                    }
+    
+    
+                    if (0) <= y && y <= (3*n){
+                        if x == 0{
+                            if 0 <= z && z <= 3*n{
+                                cube_coords.push(vec![x as f64, y as f64, z as f64, 1f64])
+                            }
+                        }
+                    }
+    
+                    if (0) <= y && y <= (3*n) {
+                        if z == 0 {
+                            if 0 <= x && x <= 3*n{
+                                cube_coords.push(vec![x as f64, y as f64, z as f64, 2f64])
+                            }
+                        }
+                    }
+    
+                    if 0 <= x && x <= 3*n{
+                        if y == (0) {
+                            if 0 <= z && z <= 3*n{
+                                cube_coords.push(vec![x as f64, y as f64, z as f64, 3f64])
+                            }
+                        }
+                    }
+    
+    
+                    if (0) <= y && y <= (3*n) {
+                        if x == 0{
+                            if 0 <= z && z <= 3*n{
+                                cube_coords.push(vec![x as f64, y as f64, z as f64, 4f64])
+                            }
+                        }
+                    }
+    
+                    if (0) <= y && y <= (3*n) {
+                        if z == 0 {
+                            if 0 <= x && x <= 3*n{
+                                cube_coords.push(vec![x as f64, y as f64, z as f64, 5f64])
+                            }
+                        }
+                    }
+                    
+
+                    
+
+
+
                 }
             }
         }
@@ -71,7 +149,7 @@ fn cube_coords() -> Vec<Vec<f64>> {
 }
 
 #[allow(non_snake_case)]
-fn update_R3(frame_n: i64, cube: Vec<Vec<f64>>) -> Vec<Vec<i64>> {
+fn update_R3(frame_n: i64, cube: &Vec<Vec<f64>>) -> Vec<Vec<i64>> {
     let mut R3 = Vec::new();
 
     let alpha = 0.01 * frame_n as f64;
@@ -98,7 +176,7 @@ fn update_R3(frame_n: i64, cube: Vec<Vec<f64>>) -> Vec<Vec<i64>> {
 
 #[allow(non_snake_case)]
 fn R3_to_R2(mut R3: Vec<Vec<i64>>) -> Vec<Vec<String>>{
-    let mut R2 = vec![vec![" ".to_string() ; n*20] ; n*20];
+    let mut R2 = vec![vec![" ".to_string() ; n*21] ; n*21];
     R3.sort_by_key(|v| (*v)[2]);
     for v in R3{
         if v[0] + 10*n as i64 >= 0 && v[1] + 10*n as i64 >= 0 { 
@@ -121,7 +199,7 @@ fn R3_to_R2(mut R3: Vec<Vec<i64>>) -> Vec<Vec<String>>{
 
 #[allow(non_snake_case)]
 fn vec_to_string(R2: Vec<Vec<String>>) -> String{
-    let mut frame: String = "".to_string();
+    let mut frame: String = ".".to_string();
 
     for y in R2{
         for x in y{
@@ -132,14 +210,15 @@ fn vec_to_string(R2: Vec<Vec<String>>) -> String{
     frame
 }
 
+
 fn main(){
-    let cube: Vec<Vec<f64>> = cube_coords();
+    let cube: Vec<Vec<f64>> = cube_coords(0);
     let mut frame_n = 1;
     loop{
         print!("{}", clear::All);
-        print!("{}\r", vec_to_string(R3_to_R2(update_R3(frame_n, cube.clone()))));
+        print!("{}\r", vec_to_string(R3_to_R2(update_R3(frame_n, &cube_coords(frame_n)))));
         io::stdout().flush().unwrap(); 
-        sleep(Duration::from_millis(1));
+        sleep(Duration::from_millis(5));
         frame_n += 1; 
 
         
